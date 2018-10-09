5cf3dce635bdde1031cf8d9572e932dea6776ae1
gaevoymix <mix.gaevoy@gmail.com>
2018-10-09 23:09:33 +0300

    Updates

---
+++ precache-manifest.19f07b07ffdc5d1b7950b7c5be60f150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "0ae7db29d6f837e95d8f",
+    "url": "/contactmanager/static/js/main.0ae7db29.chunk.js"
+  },
+  {
+    "revision": "41446149e0e11dd1a7de",
+    "url": "/contactmanager/static/js/1.41446149.chunk.js"
+  },
+  {
+    "revision": "0ae7db29d6f837e95d8f",
+    "url": "/contactmanager/static/css/main.3b74b38f.chunk.css"
+  },
+  {
+    "revision": "41446149e0e11dd1a7de",
+    "url": "/contactmanager/static/css/1.6e08e646.chunk.css"
+  },
+  {
+    "revision": "4d5292d43da38b71e61efcad14f8a893",
+    "url": "/contactmanager/index.html"
+  }
+];
